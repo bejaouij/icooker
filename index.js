66feeff76583bf452b4c5898de4d387fa64ad87d
bejaouij <bejaoui.je@mail.com>
2019-05-15 11:41:25 +0200

    feat($index): add index file behaviour
    Add the MVC index file behaviour.

---
+++ index.js
@@ -0,0 +1,25 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+let routing = require('./app/routes/web');
+
+var server = http.createServer(function(req, res) {
+	let page = url.parse(req.url).pathname;
+	let params = querystring.parse(url.parse(req.url).query);
+	let body = '';
+
+    req.on('data', function(chunk) {
+        body += chunk.toString();
+    });
+
+    req.on('end', function() {
+        routing(page, req.method, params, body, function(response) {
+			res.writeHead(response.httpCode, {'Content-Type': response.contentType});
+			res.end(response.content);
+		});
+    });
+
+	
+});
+
+server.listen(8000);
